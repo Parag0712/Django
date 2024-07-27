357ea0ab1125de3d0e896429828eedf53b4b9903
parag0712 <paragvadgama123@gmail.com>
2024-07-28 02:51:30 +0530

    relation model

--- chai/admin.py
+++ chai/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import ChaiVarity,Notes
+from .models import ChaiVarity,Notes,Certificate,ChaiReviw,Store
+class ChaiReviewInline(admin.TabularInline):
+    model = ChaiReviw
+    extra = 2
 
-admin.site.register(ChaiVarity)
-admin.site.register(Notes)
+class ChaiVarityAdmin(admin.ModelAdmin):
+    list_display = ('name','type','date_added')
+    inlines = [ChaiReviewInline]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','location')
+    filter_horizontal = ('chai_varity',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai','certificate_number')
+
+class NotesAdmin(admin.ModelAdmin):
+    list_display = ('name','description')
+    
+admin.site.register(ChaiVarity,ChaiVarityAdmin)
+admin.site.register(Store,StoreAdmin)
+admin.site.register(Notes,NotesAdmin)
+admin.site.register(Certificate,ChaiCertificateAdmin)
--- chai/models.py
+++ chai/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User 
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 # Create your models here.
+# ChaiVarity
 class ChaiVarity(models.Model):
     TYPE_CHOICE = [
         ('ML', 'MASALA'),
@@ -17,8 +21,42 @@ class ChaiVarity(models.Model):
     def __str__(self):
         return self.name
 
+# Notes Model
 class Notes(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     def __str__(self):
         return self.name
+
+# One to Many
+class ChaiReviw(models.Model):
+    chai = models.ForeignKey(ChaiVarity,on_delete=models.CASCADE,related_name='reviews')
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    rating = models.IntegerField(
+        validators=[    
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    comment = models.TextField()
+    date_add = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return f"{self.user.username} review for {self.chai.name}" 
+
+# Many to Many
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varity = models.ManyToManyField(ChaiVarity,related_name='stores')
+    def __str__(self):
+        return self.name
+
+# one to one
+class Certificate(models.Model):
+    chai = models.OneToOneField(ChaiVarity,on_delete=models.CASCADE,related_name="certificate")
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    validate_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"Certificate for {self.name}"
